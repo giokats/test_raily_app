99da00ebbc30cf9744e82f7ea18c3c6ec00ee94b
Rails <gdkatsis@gmail.com>
2017-06-03 12:19:43 +0000

    Add association between user and articles and debugger view

--- db/migrate/20170520115823_add_description_to_articles.rb
+++ db/migrate/20170520115823_add_description_to_articles.rb
@@ -1,7 +1,6 @@
 class AddDescriptionToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :description, :text
-    add_column :article, :created_at, :datetime
-    add_column :article, :updated_at, :datetime
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528143812) do
+ActiveRecord::Schema.define(version: 20170530193756) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
-
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
