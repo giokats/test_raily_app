863f893059b04f2a77f3f56a5324bd22494a06bb
Rails <gdkatsis@gmail.com>
2017-06-03 14:14:42 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :articles
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,6 +10,10 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
